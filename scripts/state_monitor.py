30682f50aea358b64dd9d5d5cbe07755913da7cd
Jad Wehbeh <jadwehbeh97@gmail.com>
2022-03-23 14:02:45 -0400

    Add state monitor node

---
+++ scripts/state_monitor.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+# State monitor node for qforge_ros package
+# Publishes Int16 current zone to 'current_zone'
+# Publishes Bool altitude warning at 'alt_state'
+# Subscribes to PoseWithCovarianceStamped vehicle position at 'mavros/global_position/local'
+
+import rospy
+from std_msgs.msg import Bool
+from std_msgs.msg import Int16
+from geometry_msgs.msg import PoseWithCovarianceStamped
+
+# Fetch node rate parameter
+monitor_rate = rospy.get_param('monitor_rate', 5)
+min_height = rospy.get_param('min_height', 2.)
+max_height = rospy.get_param('max_height', 10.)
+
+# Initialize state parameters
+alt_state = False
+current_zone = 1
+
+def pose_callback(msg):
+    # Update 'alt_state' when topic is published
+
+    global alt_state
+    global current_zone
+
+    height = msg.PoseWithCovariance.pose.position.z
+
+    if ((height < min_height) or (height > max_height)):
+        alt_state = False
+    else:
+        alt_state = True
+
+    current_zone = 1
+
+def state_monitor():
+
+    # Initialize node
+    rospy.init_node('state_monitor')
+    rate = rospy.Rate(monitor_rate)
+
+    # Define vehicle altitude status and zone publishers
+    alt_pub = rospy.Publisher('alt_state', Bool, queue_size = 1)
+    zone_pub = rospy.Publisher('current_zone', Int16, queue_size = 1)
+
+    # Define pose subscriber
+    pose_sub = rospy.Subscriber('mavros/global_position/local',PoseWithCovarianceStamped,pose_callback)
+
+    while not rospy.is_shutdown():
+
+        alt_pub.publish(alt_state)
+        zone_pub.publish(current_zone)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        state_monitor()
+    except rospy.ROSInterruptException:
+        pass
+
+
+
+
+
+
+
+
+
