81292942c5f1c1e7b9f1b58a0f2b6017d5666e82
jchernanr <jchernanr24@gmail.com>
2022-03-31 02:18:53 -0400

    Test counter variables

--- scripts/sweep_demo.py
+++ scripts/sweep_demo.py
@@ -31,16 +31,23 @@ def pose_callback(msg):
     current_pose.pose = msg.pose.pose
     current_pose.header = msg.header
 
+
+
 def sweeper():
     global setpoint_pose
+    global current_pose
+    global sweep_status 
 
     # Initialize node
     rospy.init_node('sweeper')
     rate = rospy.Rate(sweeper_rate)
-    sweep_status = 0
+    
     altitude_counter = 0
+    error_flag = False
+    setpoint_step = 2
+    sweep_status = 0
 
-    # Define vehicle state and camera subscribers
+    # Define vehicle position subscriber
     pose_sub = rospy.Subscriber('mavros/global_position/local', Odometry, pose_callback)
 
     # Define target waypoint publisher
@@ -50,15 +57,32 @@ def sweeper():
     publish_target = False
     last_msg = rospy.Time.now()
 
+
     while not rospy.is_shutdown():
-        
-        if sweep_status == 0:
-            now = rospy.Time.now()            
-            if (now.secs - last_msg.secs > 5.):
-                publish_target = True 
+        now = rospy.Time.now()        
+        rospy.Subscriber('mavros/global_position/local', Odometry, pose_callback)
+        vehicle_position = array([current_pose.pose.x,current_pose.pose.y,current_pose.pose.z])
+
+        if sweep_status == 0:                
             setpoint_pose.pose.position.x = zone2_init[0]
             setpoint_pose.pose.position.y = zone2_init[1]
             setpoint_pose.pose.position.z = altitudes[0]
+            reference_position = array([setpoint_pose.pose.position.x,setpoint_pose.pose.position.y,setpoint_pose.pose.position.z])
+            position_error = vehicle_position-reference_position
+            error_mag = linalg.norm(position_error)
+            if error_mag < 0.2:
+                sweep_status = 1
+            
+        elif sweep_status == 1:
+            setpoint_pose.pose.position.x = corners[0,0]
+            setpoint_pose.pose.position.y = corners[0,1]
+            setpoint_pose.pose.position.z = altitudes[0]
+            reference_position = array([setpoint_pose.pose.position.x,setpoint_pose.pose.position.y,setpoint_pose.pose.position.z])
+
+
+            if (now.secs - last_msg.secs > 2.):
+                publish_target = True 
+            
 
         
         if publish_target:
