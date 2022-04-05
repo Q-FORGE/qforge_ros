f66e6266e562bf6b697aad76d470da7e9a10c314
Zihao Zhuo <zhzhuo@outlook.com>
2022-04-05 13:41:32 -0400

    add launch trajectory server

---
+++ scripts/launch_trajectory.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# service node to determine trajectory for launch operation
+
+from __future__ import print_function
+
+import rospy
+import numpy as np
+from math import pi, modf
+from scipy.spatial.transform import Rotation as R
+import std_msgs.msg
+from geometry_msgs.msg import Transform,Quaternion,Twist,Vector3,PoseStamped,Pose,Point
+from trajectory_msgs.msg import MultiDOFJointTrajectory, MultiDOFJointTrajectoryPoint
+from qforge_ros.srv import LaunchTrajectory, LaunchTrajectoryResponse
+
+#x_offset = 0.8
+x_offset = 0.3
+z_offset = 0.4
+x_buffer = 3
+z_buffer = 1
+t_buffer = 2
+a = 2.75
+g = -9.81
+
+def calculate_launch_trajectory(req):
+    target_position = np.array([req.target_position.x,req.target_position.y,req.target_position.z])
+    wall_normal = np.array([req.wall_normal.x,req.wall_normal.y,req.wall_normal.z])
+
+    psi = np.arctan2(-wall_normal[1],-wall_normal[0])
+    r = R.from_euler('z', [psi])
+    HDG_quat = r.as_quat()
+    
+    # target height
+    z_t = target_position[2]
+    # release height to achieve z_m
+    z_m = -(a*z_t-8*a-4*g)/(a+g)
+    # vertical velocity at release
+    w_m = np.sqrt(2*a*(4-z_m))
+    # max time from release to reach target height
+    t_m = -(np.sqrt(2*g*(z_t-z_m)+2*a*(4-z_m))+np.sqrt(2*a*(4-z_m)))/g
+    # release position to achieve max x travel with travel time of t_m 
+    x_m = (a*t_m**2)/2
+    # horizontal velocity at release
+    u_m = np.sqrt(2*a*x_m)
+    #t_ready = 5
+    t_accel = max(w_m,u_m)/a+t_buffer
+    t_decel = max(w_m,u_m)/a
+    
+    release_position = (x_offset+x_m)*wall_normal+np.array([target_position[0],target_position[1],z_m+z_offset])
+    release_velocity = np.array([u_m,0,w_m])
+
+    release_point = MultiDOFJointTrajectoryPoint()
+    release_point.transforms = [Transform(translation=Vector3(release_position[0],release_position[1],release_position[2]),\
+        rotation=Quaternion(HDG_quat[0,0],HDG_quat[0,1],HDG_quat[0,2],HDG_quat[0,3]))]
+    release_point.velocities = [Twist(linear=Vector3(release_velocity[0],release_velocity[1],release_velocity[2]),angular=Vector3(0,0,0))]
+    release_point.accelerations = [Twist(linear=Vector3(a*wall_normal[0],a*wall_normal[1],-a),angular=Vector3(0,0,0))]
+    release_point.time_from_start = rospy.Duration.from_sec(t_accel)
+
+    #start_position = release_position + (u_m**2/(2*a))*wall_normal - (w_m**2/(2*a))*np.array([0,0,1])
+    start_position = release_position + (u_m**2/(2*a)+x_buffer)*wall_normal - (w_m**2/(2*a)+z_buffer)*np.array([0,0,1])
+    # start_point = MultiDOFJointTrajectoryPoint()
+    # start_point.transforms = [Transform(translation=Vector3(start_position[0],start_position[1],start_position[2]),\
+    #     rotation=Quaternion(HDG_quat[0,0],HDG_quat[0,1],HDG_quat[0,2],HDG_quat[0,3]))]   
+    # start_point.time_from_start = rospy.Duration.from_sec(t_ready)
+    start_point = PoseStamped()
+    start_point.pose = Pose(position=Point(start_position[0],start_position[1],start_position[2]),\
+        orientation=Quaternion(HDG_quat[0,0],HDG_quat[0,1],HDG_quat[0,2],HDG_quat[0,3]))
+
+    #end_position = release_position + (u_m**2/(2*a))*wall_normal + (w_m**2/(2*a))*np.array([0,0,1])
+    end_position = release_position + (u_m**2/(2*a)+x_buffer)*wall_normal + (w_m**2/(2*a))*np.array([0,0,1])
+    end_point = MultiDOFJointTrajectoryPoint()
+    end_point.transforms = [Transform(translation=Vector3(end_position[0],end_position[1],end_position[2]),\
+        rotation=Quaternion(HDG_quat[0,0],HDG_quat[0,1],HDG_quat[0,2],HDG_quat[0,3]))]
+    end_point.time_from_start = rospy.Duration.from_sec(t_accel+t_decel)
+
+    response = LaunchTrajectoryResponse()
+    response.start_point = start_point
+    response.trajectory = MultiDOFJointTrajectory()
+    response.trajectory.points = [release_point,end_point]
+    
+    return response
+
+def launch_trajectory_server():
+    rospy.init_node('launch_trajectory_server')
+    s = rospy.Service('launch_trajectory', LaunchTrajectory, calculate_launch_trajectory)
+    rospy.spin()
+
+if __name__ == "__main__":
+    try:
+        launch_trajectory_server()
+    except rospy.ROSInterruptException:
+        pass
+    
