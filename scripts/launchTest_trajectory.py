10b5d4a12c6b8114866b36cf48ad4b817dbf0e24
Zihao Zhuo <zhzhuo@outlook.com>
2022-04-05 15:19:40 -0400

    add test node for launch_trigger

--- scripts/launchTest_trajectory.py
+++ scripts/launchTest_trajectory.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
-# use to test ball launch operation
+# use to test ball launch trajectory
 
 import rospy
-import sys
 from geometry_msgs.msg import Point,Vector3,PoseStamped
 from trajectory_msgs.msg import MultiDOFJointTrajectory,MultiDOFJointTrajectoryPoint
 from qforge_ros.srv import LaunchTrajectory, LaunchTrajectoryRequest
 
 
-def launch():
+def launchTest_trajectory():
     rospy.wait_for_service('launch_trajectory')
     launch_trajectory_handle = rospy.ServiceProxy('launch_trajectory',LaunchTrajectory)
     launch_trajectory_input = LaunchTrajectoryRequest()
@@ -16,7 +15,7 @@ def launch():
     launch_trajectory_input.wall_normal = Vector3(-1,0,0)
     launch_trajectory = launch_trajectory_handle(launch_trajectory_input)
     
-    rospy.init_node('launch_test')
+    rospy.init_node('launchTest_trajectory')
 
     start_point_pub = rospy.Publisher('/red/tracker/input_pose', PoseStamped, queue_size=1, latch=True)
     trajectory_pub = rospy.Publisher('/red/tracker/input_trajectory', MultiDOFJointTrajectory, queue_size=1, latch=True)
@@ -29,7 +28,7 @@ def launch():
 
 if __name__ == '__main__':
     try:
-        launch()
+        launchTest_trajectory()
     except rospy.ROSInterruptException:
         pass
 
--- scripts/launch_trigger.py
+++ scripts/launch_trigger.py
@@ -3,27 +3,56 @@
 
 import rospy
 import numpy as np
-from math import pi
 from scipy.spatial.transform import Rotation as R
-import std_msgs.msg
-from nav_msgs.msg import Odometry
-from geometry_msgs.msg import Transform,Quaternion,Twist,Vector3,PoseStamped,Pose,Point
+from geometry_msgs.msg import Vector3
 from qforge_ros.srv import LaunchTrigger, LaunchTriggerResponse
 
-ball_position = np.array([[0,0,0]])
-ball_velocity = np.array([[0,0,0]])
-
-
-def odometry_callback(msg):
-    global ball_position,ball_velocity
-    ball_position = np.array([[msg.pose.pose.position.x,msg.pose.pose.position.y,msg.pose.pose.position.z]])
-    r = R.from_quat([msg.pose.pose.orientation.x,msg.pose.pose.orientation.y,msg.pose.pose.orientation.z,msg.pose.pose.orientation.w])
-    ball_velocity = r.apply([[msg.twist.twist.linear.x,msg.twist.twist.linear.y,msg.twist.twist.linear.z]])
-
+g = -9.81;
+t_end = 5; # check ball trajectry in the next t_end second
+lon_tolerance = 0.2;
+lat_tolerance = 0.2;
+response = LaunchTriggerResponse()
 
 def calculate_launch_trigger(req):
-    target_position = np.array([req.target_position.x,req.target_position.y,req.target_position.z])
-    wall_normal = np.array([req.wall_normal.x,req.wall_normal.y,req.wall_normal.z])
+    ball_position = np.array([[req.odometry.pose.pose.position.x,req.odometry.pose.pose.position.y,\
+        req.odometry.pose.pose.position.z]])
+    r = R.from_quat([req.odometry.pose.pose.orientation.x,req.odometry.pose.pose.orientation.y,\
+        req.odometry.pose.pose.orientation.z,req.odometry.pose.pose.orientation.w])
+    ball_velocity = r.apply([[req.odometry.twist.twist.linear.x,req.odometry.twist.twist.linear.y,\
+        req.odometry.twist.twist.linear.z]])
+    
+    target_position = np.array([[req.target_position.x,req.target_position.y,req.target_position.z]])
+    wall_normal = np.array([[req.wall_normal.x,req.wall_normal.y,req.wall_normal.z]])
+
+    ball_position_end  = ball_position + ball_velocity*t_end + np.array([[0,0,1]])*(0.5*g*t_end**2)
+
+    if ((ball_position_end-target_position)@wall_normal.T)<=0:
+        # end point on the other side of the wall
+        # calculation impact point
+        t_impact = ((ball_position-target_position)@wall_normal.T)/(-ball_velocity@wall_normal.T)
+        ball_position_impact = ball_position + ball_velocity*t_impact +\
+            np.array([[0,0,1]])*(0.5*g*t_impact**2)
+        delivery_error = ball_position_impact - target_position
+
+        response.error = Vector3(delivery_error[0,0],delivery_error[0,1],delivery_error[0,2])
+        if abs(delivery_error@np.array([[0,0,1]]).T)<lon_tolerance:
+            response.trigger = True
+        else:
+            response.trigger = False
+    else:
+        # does not reach wall in t_end seconds
+        response.error = Vector3(np.NaN,np.NaN,np.NaN)
+        response.trigger = False
+
+    return response
 
 def launch_trigger_server():
     rospy.init_node('launch_trigger_server')
+    s = rospy.Service('launch_trigger', LaunchTrigger, calculate_launch_trigger)
+    rospy.spin()
+
+if __name__ == "__main__":
+    try:
+        launch_trigger_server()
+    except rospy.ROSInterruptException:
+        pass
