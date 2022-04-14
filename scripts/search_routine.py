b2c4985443ab71f1b5bda5fb3fd050a6e47bed0b
jchernanr <jchernanr24@gmail.com>
2022-04-13 23:29:16 -0400

    Configure basic service response

--- scripts/search_routine.py
+++ scripts/search_routine.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# service for area 3 sweep and AR search
+# service for area 3 sweep 
 
 import rospy
 import numpy as np
@@ -8,5 +8,35 @@ from scipy.spatial.transform import Rotation as R
 import std_msgs.msg
 from geometry_msgs.msg import Transform,Quaternion,Twist,Vector3,PoseStamped,Pose,Point
 from trajectory_msgs.msg import MultiDOFJointTrajectory, MultiDOFJointTrajectoryPoint
+from qforge_ros.srv import SearchRoutine, SearchRoutineResponse
 
+def calculate_search_trajectory(req):
+    
+    initial_pose = MultiDOFJointTrajectoryPoint()
+    initial_pose.transforms = [Transform(translation=Vector3(2,0,2),rotation=Quaternion(0,0,0,1))]
+    initial_pose.time_from_start = 2
+
+    corner_1_pose = MultiDOFJointTrajectoryPoint()
+    corner_1_pose.transforms = [Transform(translation=Vector3(4,2,8),rotation=Quaternion(0,0,0,1))]
+    corner_1_pose.time_from_start = 2
+
+    # search_trajectory = MultiDOFJointTrajectory()
+    # search_trajectory.points = [initial_pose, corner_1_pose]
+
+    response = SearchRoutineResponse()
+    response.trajectory = MultiDOFJointTrajectory()
+    response.trajectory.points = [initial_pose, corner_1_pose]
+    
+    return response
+
+def search_routine_server():
+    rospy.init_node('search_routine_server')
+    s = rospy.Service('search_routine', SearchRoutine, calculate_search_trajectory)
+    rospy.spin()
+
+if __name__ == "__main__":
+    try:
+        search_routine_server()
+    except rospy.ROSInterruptException:
+        pass
 
--- scripts/search_routine_tester.py
+++ scripts/search_routine_tester.py
@@ -4,9 +4,34 @@
 import rospy
 from geometry_msgs.msg import Point,Vector3,PoseStamped
 from trajectory_msgs.msg import MultiDOFJointTrajectory,MultiDOFJointTrajectoryPoint
-# from qforge_ros.srv import LaunchTrajectory, LaunchTrajectoryRequest
+from qforge_ros.srv import SearchRoutine, SearchRoutineRequest
 
 
 
-message_test = SearchRoutineResponse
+def search_routine_Tester():
+    rospy.wait_for_service('search_routine')
+    search_routine_handle = rospy.ServiceProxy('search_routine',SearchRoutine)
+    search_routine_input = SearchRoutineRequest()
+    search_routine_input.x_bound = [2,12]
+    search_routine_input.y_bound = [-6,6]
+    search_routine_input.z_bound = [2,6]
+    search_routine_input.fov = [4,4,1]
+    search_routine_input.speed = 3
+    search_routine = search_routine_handle(search_routine_input)
+
+    rospy.init_node('searh_routine_Tester')
+
+    trajectory_pub = rospy.Publisher('/red/tracker/input_trajectory', MultiDOFJointTrajectory, queue_size=1, latch=True)
+    trajectory_pub.publish(search_routine.trajectory)
+    rospy.sleep(5)
+
+if __name__ == '__main__':
+    try:
+        search_routine_Tester()
+    except rospy.ROSInterruptException:
+        pass
+
+
+
+# message_test = SearchRoutineResponse
 
