5081a92de007e7fc771def21dfe5b0dd9ad6116a
Jad Wehbeh <jadwehbeh97@gmail.com>
2022-06-22 08:38:12 -0400

    Decrease ball drop delay

--- scripts/launch_gunner.py
+++ scripts/launch_gunner.py
@@ -14,7 +14,7 @@ launch_gunner_rate = rospy.get_param('launch_gunner_rate',150)
 # initialize variables
 state = String()
 info_received = False
-delay = 0.25 # seconds
+delay = 0.15 # seconds
 
 def state_callback(msg):
     # fetch state from commander
