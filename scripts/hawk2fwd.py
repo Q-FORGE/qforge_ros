97df2a577a82cdeb838baca23bade800e8767f5b
Jackson <15jgme@queensu.ca>
2022-06-22 10:41:32 +0200

    add conversion node

---
+++ scripts/hawk2fwd.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+# Commander node for qforge_ros package
+# Publishes estimated artag location 'ar_tag_est'
+# Subscribes to ar tag measurment 'ar_point'
+# Subscribes to odometry '/red/odometry'
+
+import rospy
+from scripts.pathfinder import odometry_callback
+from nav_msgs.msg import Odometry
+
+global odom_pub 
+odom_pub = rospy.Publisher('odometry', Odometry, queue_size = 1)
+
+def odometry_callback(msg):
+    msg.header.frame_id = "optitrack"
+    odom_pub.publish(msg)
+    
+
+
+def arTag():
+    rospy.Subscriber('/hawk2/vrpn_client/estimated_odometry', Odometry, odometry_callback, queue_size=1, buff_size=2**24)
+
+    while not rospy.is_shutdown():
+
+
+if __name__ == '__main__':
+    try:
+        arTag()
+    except rospy.ROSInterruptException:
+        pass
