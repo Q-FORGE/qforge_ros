4b2a92dda05cb4c8731e74a403d4494bcd9339e1
Jad Wehbeh <jadwehbeh97@gmail.com>
2022-06-23 03:47:08 -0400

    Fix bagged topics

--- tools/bag_run.sh
+++ tools/bag_run.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-rosbag record -O qforge_test.bag /red/vehicle_state /red/ar_tag_est /red/odometry /red/tracker/input_pose /red/pathfinder/trajectory_vis /red/launch/gunner_status /red/launch/error /red/launch/start_pose  __name:=qforge_bagger
+rosbag record -O qforge_test.bag /red/vehicle_state /red/ar_tag_est /hawk2/vrpn_client/estimated_odometry /red/tracker/input_pose /red/pathfinder/trajectory_vis /red/launch/gunner_status /red/launch/error /red/launch/start_pose  __name:=qforge_bagger
